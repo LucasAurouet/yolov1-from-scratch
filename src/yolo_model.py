e0cb0cbb03e80308856ed6feb3cc59172e6e019a
JadenDoesML <lucas.aurouet@gmail.com>
2025-12-17 12:14:37 +0100

    solved a bug related to the batch_size parameter

--- src/yolo_model.py
+++ src/yolo_model.py
@@ -1,11 +1,12 @@
 import torch.nn as nn
 
 class YOLOv1(nn.Module):
-    def __init__(self, S=7, B=2, C=52):
+    def __init__(self, S=7, B=2, C=52, batch_size=16):
         super().__init__()
         self.S = S
         self.B = B
         self.C = C
+        self.batch_size = batch_size
         
         self.features = nn.Sequential(
             nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1),
@@ -49,11 +50,10 @@ class YOLOv1(nn.Module):
     
     def forward(self, x):
         # forward pass through the network
-        batch_size = x.shape[0]
         x = self.features(x)
         x = self.fc(x)
         # reshape output to (N, S, S, C+B*5) grid-like format
-        x = x.view(batch_size, self.S, self.S, self.C + self.B * 5)
+        x = x.view( self.batch_size, self.S, self.S, self.C + self.B * 5)
         
         return x
 
